0a3fa7829cdf9c3fb20e5235cca92c21d8f22919
Juan <juan.notario20@gmail.com>
2018-02-14 09:16:01 +0100

    Commit inicial

---
+++ src/entornos/Entornos.java
@@ -0,0 +1,79 @@
+package entornos;
+
+public class Entornos {
+	private String nombre;
+	private int edad;
+	private String dni;
+	private char sexo = 'H';
+	private int peso;
+	private double altura;
+	
+	public Persona() {}
+	
+	
+	public Persona(String nombre, int edad, String dni, char sexo) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		this.sexo = sexo;
+	}
+
+	public Persona(String nombre, int edad, String dni, char sexo, int peso, double altura) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		this.sexo = sexo;
+		this.peso = peso;
+		this.altura = altura;
+		
+	}
+		
+		public double calcularIMC() {
+			double IMC = this.peso / (this.altura * this.altura);
+			double estado = (IMC < 18)? -1 : (IMC > 25)? 1 : 0;
+			return estado;
+			
+		}
+		public boolean esMayorDeEdad() {
+			boolean edad = (this.edad > 18)? true : false;
+			return edad;
+		}	
+		public char comprobarSexo() {
+			char sexo = (this.sexo == 'H')? 'H' : 'H';
+			return sexo;
+		}
+		
+		public String toString() {
+			String sPersona = "Nombre :  " + this.nombre
+			+ " Edad: " + this.edad + " DNI: " + this.dni + " sexo: " + this.sexo
+			+ " Peso: " + this.peso + " Altura: " + this.altura;
+			return sPersona;
+			
+		
+		}
+
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+
+		public void setSexo(char sexo) {
+			this.sexo = sexo;
+		}
+
+
+		public void setPeso(int peso) {
+			this.peso = peso;
+		}
+
+
+		public void setAltura(double altura) {
+			this.altura = altura;
+		}
+}
